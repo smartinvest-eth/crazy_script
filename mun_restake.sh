efa5c445fc440861dc98ad310a03a723bbc2021a
VCFAdvisor <41657478+VCFAdvisor@users.noreply.github.com>
2022-10-07 09:01:33 +0700

    Update mun_restake.sh

--- mun_restake.sh
+++ mun_restake.sh
@@ -5,10 +5,10 @@ for (( ;; )); do
 	echo -e "\033[0;32mWaiting 30 seconds before requesting balance\033[0m"
 	sleep 30
 	AMOUNT=$(mund q bank balances $(mund keys show wallet -a) | grep amount | awk '{split($0,a,"\""); print a[2]}')
-	AMOUNT=$(($AMOUNT - 500))
+	AMOUNT=$(($AMOUNT - 5000))
 	AMOUNT_STRING=$AMOUNT"utmun"
 	echo -e "Your total balance: \033[0;32m$AMOUNT_STRING\033[0m"
 	 mund tx staking delegate $(mund keys show wallet --bech val -a) $AMOUNT_STRING --from wallet --chain-id testmun -y
 	echo -e "\033[0;32m$AMOUNT_STRING staked! Restarting in 3600 sec!\033[0m"
-	sleep 3600
+	sleep 7200
 done
